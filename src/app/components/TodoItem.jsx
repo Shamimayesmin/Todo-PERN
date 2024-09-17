700469c831a3d68720f03d340deaaa22e3a2e43a
Shamimayesmin <shamimayesmin144@gmail.com>
2024-09-17 17:38:34 +0600

    new added

--- src/app/components/TodoItem.jsx
+++ src/app/components/TodoItem.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const TodoItem = () => {
-    return (
-        <>
-        <h1 className='text-center mt-11 mb-5 text-2xl text-sky-600 font-bold font-serif'>Input TODO</h1>
-        <form className='w-1/2 mx-auto flex flex-col items-center p-4'>
-            <input type="text" 
-            placeholder='input todo'
-            className='w-1/2 p-3 text-center rounded-md border-2'
-             />
-        </form>
-            
-        </>
-    );
+	return (
+		<>
+			<h1 className="text-center mt-11 mb-5 text-2xl text-sky-600 font-bold font-serif">
+				Input TODO
+			</h1>
+			<div className="w-1/2 mx-auto">
+				<form className=" p-4 flex justify-center gap-3">
+					<input
+						type="text"
+						placeholder="input todo"
+						className="w-1/2 p-3 text-center rounded-md border-2"
+					/>
+					<button className="px-4 bg-blue-500 text-white rounded-md">
+						Add
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default TodoItem;
--- src/app/components/TodoList.jsx
+++ src/app/components/TodoList.jsx
@@ -3,25 +3,28 @@ import React from "react";
 const TodoList = () => {
 	return (
 		<>
-			<div className="overflow-x-auto w-1/2 mx-auto flex flex-col items-center">
+			<div className="overflow-x-auto w-1/2 mx-auto flex flex-col items-center mt-14">
 				<table className="table">
 					{/* head */}
 					<thead>
-						<tr>
+						<tr className="text-base">
 							<th></th>
 							<th>Title</th>
-							<th>Edit</th>
-							<th>Delete</th>
+							<th>Actions</th>
+							{/* <th>Delete</th> */}
 						</tr>
 					</thead>
 					<tbody>
-					
-
-						<tr className="">
-							<th>2</th>
-							<td>Hart Hagerty</td>
-							<td>Desktop Support Technician</td>
-							<td>Purple</td>
+						<tr>
+							<th className="p-5">2</th>
+							<td>Hart Hagerty gggggggggggggg</td>
+							
+							<th className="w-20 p-1">
+								<button className="btn btn-ghost btn-sm bg-sky-400">Edit</button>
+							</th>
+							<th className="w-20 p-1">
+								<button className="btn btn-ghost btn-sm bg-green-400">Delete</button>
+							</th>
 						</tr>
 					</tbody>
 				</table>
